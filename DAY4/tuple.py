0e2721fc0bd8e3223b029092d306f2199b526441
IWATA JULIANA <mari.i@gainsline.com>
2021-09-27 14:57:44 +0900

    tuple  practice

--- DAY4/tuple.py
+++ DAY4/tuple.py
@@ -1,10 +1,19 @@
-from typing import Tuple
+# from typing import Tuple
 
+# print(type((1,2,3)))
 
-print(type((1,2,3)))
+# tuple = ('toda','kanai')
+# print(tuple[0])
 
-tuple = ('toda','kanai')
-print(tuple[0])
+# tuple[0] = 'juliana'
+# #tupleは中の内容の更新ができない
 
-tuple[0] = 'juliana'
-#tupleは中の内容の更新ができない
+list = ['abc',1,'100']
+print(list[0],list[1],list[2])
+list[1] = 20
+print(list[1])
+
+tuple = ('abc',1,'100')
+print(tuple[0],tuple[1],tuple[2])
+tuple[1] = 20
+print(tuple[1])
