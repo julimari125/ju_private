c0dc2b217c20b5757ade1c7ee6e2a9d12b533a79
IWATA JULIANA <mari.i@gainsline.com>
2021-09-27 15:00:19 +0900

    fetch_all  practice

--- DAY4/connect_sqlite.py
+++ DAY4/connect_sqlite.py
@@ -10,10 +10,12 @@ args = sys.argv
 db_name = 'python_training.db'
 connection = sqlite3.connect(db_name)
 cursor = connection.cursor()
+#追加部分
 books = cursor.execute('SELECT * FROM books').fetchall()
-print(books)
-book = cursor.execute('SELECT * FROM books').fetchone()
-print(book)
-connection.execute(f"INSERT INTO books(name,kind)VALUES ('{args[1]}','{args[2]}')")
+for b in books:
+    print(b)
+# book = cursor.execute('SELECT * FROM books').fetchone()
+# print(book)
+# connection.execute(f"INSERT INTO books(name,kind)VALUES ('{args[1]}','{args[2]}')")
 connection.commit()
 connection.close()
