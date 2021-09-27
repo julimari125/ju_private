5af56984474c5817a313b0e24500507c9e35a927
IWATA JULIANA <mari.i@gainsline.com>
2021-09-27 16:02:07 +0900

    flask practice

--- DAY5/flask_controller.py
+++ DAY5/flask_controller.py
@@ -1,10 +1,17 @@
 from flask import Flask,render_template
+import sqlite3
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html', book = 'ぼっちゃっん')
+    db_name = 'python_training.db'
+    connection = sqlite3.connect(db_name)
+    cursor = connection.cursor()
+    sql = 'SELECT * from books'
+    book = cursor.execute(sql).fetchone()
+    connection.close
+    return render_template('index.html', book = book)
 
 @app.route('/hello')
 def hello():
