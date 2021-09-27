3af0c46eda4f884bf1222d317094be8b6e423b14
IWATA JULIANA <mari.i@gainsline.com>
2021-09-27 17:12:07 +0900

    flask practice

--- DAY5/flask_controller.py
+++ DAY5/flask_controller.py
@@ -6,17 +6,15 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     parameter = request.args
-    print(parameter.get('name'))
     book_name = parameter.get('name','')
     book_kind = parameter.get('kind','')
     db_name = 'python_training.db'
     connection = sqlite3.connect(db_name)
     cursor = connection.cursor()
-    sql = 'SELECT * from books'
+    sql = f"SELECT * from books WHERE name LIKE '%{book_name}%' AND kind LIKE '%{book_kind}%'"
     books = cursor.execute(sql).fetchall()
-    print(books)
     connection.close
-    return render_template('index.html', book = book_name, kind = book_kind )
+    return render_template('index.html', books = books )
 
 @app.route('/hello')
 def hello():
