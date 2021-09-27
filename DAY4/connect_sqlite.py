faaf5f536f983e6c39b3366a86434796b691a313
IWATA JULIANA <mari.i@gainsline.com>
2021-09-27 14:47:26 +0900

    sqlite practice

--- DAY4/connect_sqlite.py
+++ DAY4/connect_sqlite.py
@@ -1,7 +1,19 @@
 import sqlite3
+import sys
+# db_name = 'python_training.db'
+# connection = sqlite3.connect(db_name)
+# cursor = connection.cursor()
+# cursor.execute('INSERT INTO books (name, kind) VALUES ("python入門","プログラミング")')
+# connection.commit()
+# connection.close()
+args = sys.argv
 db_name = 'python_training.db'
 connection = sqlite3.connect(db_name)
 cursor = connection.cursor()
-cursor.execute('INSERT INTO books (name, kind) VALUES ("python入門","プログラミング")')
+books = cursor.execute('SELECT * FROM books').fetchall()
+print(books)
+book = cursor.execute('SELECT * FROM books').fetchone()
+print(book)
+connection.execute(f"INSERT INTO books(name,kind)VALUES ('{args[1]}','{args[2]}')")
 connection.commit()
 connection.close()
