cd70a0de325cbe27b661cf3df2a50adb70831600
IWATA JULIANA <mari.i@gainsline.com>
2021-09-28 14:41:22 +0900

    java script practice

---
+++ DAY5/script/getNode.js
@@ -0,0 +1,20 @@
+//idで取得
+
+document.getElementById("itroduction");
+
+//タグ名で取得
+
+document.getElementsByClassTagName("p");
+
+
+// クラス名で取得
+
+document.getElementsByClassName("red");
+
+//CSSクラスタで一つ取得
+
+document.querySelector("#title")
+
+// CSSクラスタで複数指定
+
+document.querySelectorAll(".red")
