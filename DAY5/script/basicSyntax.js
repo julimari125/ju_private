420ddf4eb56adebe3ab12b08b7d8384d0ef5035e
IWATA JULIANA <mari.i@gainsline.com>
2021-09-28 13:50:51 +0900

    java script practice

---
+++ DAY5/script/basicSyntax.js
@@ -0,0 +1,24 @@
+//変数宣言
+
+const name = 'toda';
+let status = 'active';
+var input = 10;
+
+
+//if文
+//バッククオートは option + ~ 
+
+if (status == 'active') {
+    console.log(`hello${name}`);
+    console.log(input);
+} else {
+    console.log("bye");
+}
+
+
+//for文
+
+for (seasons of ["春","夏","秋","冬"]) {
+    console.log(season);
+}
+
