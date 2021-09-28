08d3c7d97540d0a15dae6e68f4003ee58edca3d9
IWATA JULIANA <mari.i@gainsline.com>
2021-09-28 13:56:53 +0900

    java script practice

---
+++ DAY5/script/dataType.js
@@ -0,0 +1,25 @@
+// boolean
+
+console.log(typeof true);
+console.log(typeof false);
+
+
+// Number
+
+console.log(typeof 1);
+
+
+// String
+console.log(typeof "岩田")
+
+// 構造型
+// JSON (java script object notation);
+
+person = {
+    "person": {
+        "name": "iwata",
+        "age": 23
+    }
+}
+console.log(typeof person);
+
