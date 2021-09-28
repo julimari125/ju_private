d9e7734b5d1227436a6eebbab4551463d1f38416
IWATA JULIANA <mari.i@gainsline.com>
2021-09-29 08:28:44 +0900

    fix add

---
+++ DAY5/tests/fizz_buzz.py
@@ -0,0 +1,9 @@
+class FizzBuzz:
+ def convert(self, input):
+  if input % 15 == 0: 
+    return 'FizzBuzz'
+  elif input % 3 == 0: 
+    return 'Fizz'
+  elif input % 5 == 0: return 'Buzz'
+  else:
+    return input
